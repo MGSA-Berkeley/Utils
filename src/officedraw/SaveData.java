465a05d4e4604c551bfd1e05d76092a200b9e263
Thomas Browning <tb65536@uw.edu>
2022-04-16 05:01:36 -0700

    sort 1

--- src/officedraw/SaveData.java
+++ src/officedraw/SaveData.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -15,8 +16,14 @@ public class SaveData {
     public static void save(Map<Integer, Person[]> map) {
         try {
             for (int year : map.keySet()) {
+                Person[] oldpeople = map.get(year);
+                int len = oldpeople.length - 1;
+                Person[] sortedpeople = Arrays.copyOf(map.get(year), len);
+                Arrays.sort(sortedpeople, (p, q) -> p.buttons[0].getText().compareTo(q.buttons[0].getText()));
+                Person[] newpeople = Arrays.copyOf(sortedpeople, len + 1);
+                newpeople[len] = new Person();
                 List<String> lines = new ArrayList<>();
-                for (Person p : map.get(year)) {
+                for (Person p : newpeople) {
                     lines.add(p.buttons[0].getText() + tab + p.buttons[1].getText() + tab + p.buttons[2].getText() + tab + p.buttons[3].getText() + tab + p.buttons[4].getText() + tab + p.buttons[5].getText());
                 }
                 Path file = Paths.get(year + ".officedraw");
